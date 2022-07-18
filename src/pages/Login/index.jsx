1961e9ea087cd49547a6baaa83da48fd9c6b37f9
quanhui <quanhuiguo30@gmail.com>
2022-07-18 22:15:05 +0800

    axios安装配置跨域未成功

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,9 +3,19 @@ import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Form, Input } from 'antd';
 import logo from '../../static/logo.png'
 import "./login.less"
+import axios from "axios";
 export default function Login() {
     const onFinish = (values) => {
+        const { username, password } = values
         console.log('Received values of form: ', values);
+        axios({
+            method: 'post',
+            url: 'http://localhost:5000/login',
+            data: {
+                username,
+                password
+            }
+        }).then(res => { console.log(res) }).catch(err => { console.log(err); })
     };
     return (
         <div className='page-login'>
@@ -16,6 +26,7 @@ export default function Login() {
             <section>
                 <h2>用户登录</h2>
                 <Form
+                    scrollToFirstError
                     name="normal_login"
                     className="login-form"
                     initialValues={{
@@ -26,21 +37,18 @@ export default function Login() {
                     <Form.Item
                         name="username"
                         rules={[
-                            {
-                                required: true,
-                                message: 'Please input your Username!',
-                            },
+                            { required: true, message: '请输入用户名' },
+                            { max: 6, type: 'string', message: "长度不能超过6位" }
                         ]}
                     >
-                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+                        <Input hasFeedback prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
                     </Form.Item>
                     <Form.Item
                         name="password"
                         rules={[
-                            {
-                                required: true,
-                                message: 'Please input your Password!',
-                            },
+                            { required: true, message: '请输入密码' },
+                            { max: 6, type: 'string', message: "长度不能超过6位" },
+                            { pattern: /^[A-Za-z][\w]{4}/, message: '必须以字母开头长度至少5位' }
                         ]}
                     >
                         <Input
